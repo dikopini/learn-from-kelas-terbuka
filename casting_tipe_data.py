3da9533d57f0b11ba27d77a565fb16f3066daad9
Irfan Basyaruddin <irfanbasyar.ib@gmail.com>
2021-12-29 09:34:01 +0700

    casting tipe data

---
+++ casting_tipe_data.py
@@ -0,0 +1,13 @@
+# kita belajar casting
+# merubah dari satu tipe data ke tipe data yang lain
+# tipe data = int, str, float, bool
+
+data_int = 9
+print(f"data = {data_int}, type data = {type(data_int)}")
+
+data_float = float(data_int)
+data_str = str(data_int)
+data_bool = bool(data_int)
+print(f"data = {data_float}, type data = {type(data_float)}")
+print(f"data = {data_str}, type data = {type(data_str)}")
+print(f"data = {data_bool}, type data = {type(data_bool)}")
