44f2ed6008ed123f9f6234bff20552efab03bb8f
Irfan Basyaruddin <irfanbasyar.ib@gmail.com>
2022-07-27 11:18:12 +0700

    input data

---
+++ 07_mengambil_input.py
@@ -0,0 +1,11 @@
+#mengambil input dari user
+
+#data yang dimasukkan pasti string
+data = input("Masukkan data: ")
+
+print ("data =",data,", type = ", type(data))
+
+#jika kita ingin mengambil int, maka
+data_int = int(input("masukkan angka: "))
+
+print ("data_int =",data_int,", type = ", type(data_int))
