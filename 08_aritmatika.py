f99e6bfb1a0c83f7f18c4d717976a652e5fc288c
Irfan Basyaruddin <irfanbasyar.ib@gmail.com>
2022-07-27 22:26:25 +0700

    arit

---
+++ 08_aritmatika.py
@@ -0,0 +1,39 @@
+#operasi aritmatika
+
+a = 10
+b = 3
+#operasi pertambahan +
+hasil = a + b
+print(a,"+",b,"=",hasil)
+
+#operasi pengurangan -
+hasil = a - b
+print(a,"-",b,"=",hasil)
+
+#operasi perkalian *
+hasil = a * b
+print(a,"*",b,"=",hasil)
+
+#operasi pembagian /
+hasil = a / b
+print(a,"/",b,"=",hasil)
+
+#operasi eksponensial (pangkat) **
+hasil = a ** b
+print(a,"**",b,"=",hasil)
+
+#operasi modulus %
+hasil = a % b
+print(a,"%",b,"=",hasil)
+
+#operasi floor division //
+hasil = a // b
+print(a,"//",b,"=",hasil)
+
+"""
+prioritas operation
+1. ( ) langkah paling pertama
+2. exponential
+3. perkalian, pembagian, modulus, dan floor division
+4. penjumlahan dan pengurangan
+"""
